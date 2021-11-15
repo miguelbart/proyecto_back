771ab0d58b9ab9be7e0b7708c6939ccba2e8bc6f
Yoan Steiner H <71861980+yo-huaca95@users.noreply.github.com>
2021-11-14 19:28:48 -0500

    Cambios3

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppRouter from '../src/components/router/router'
+import 'react-scroll';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppRouter></AppRouter>
+    </div>
+  );
+}
+
+export default App;
