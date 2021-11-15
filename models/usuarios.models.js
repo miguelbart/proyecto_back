2bf4aee8b2a8f7e5e3b621dff92f4b0abc2205f6
Yoan Steiner H <71861980+yo-huaca95@users.noreply.github.com>
2021-11-14 19:31:20 -0500

    Update

--- models/usuarios.models.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-const UsuarioSchema =new Schema({
-    usuario:{type:String, requiere:true, max:100},
-    pass:{type:String, requiere:true, max:128},
-})
-
-module.exports= mongoose.model("usuarios",UsuarioSchema);
